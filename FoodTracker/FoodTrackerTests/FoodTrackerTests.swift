ce3e33d15ed067b1edfc569b4fc4cd7ca6fe1a49
Jai Bansal <jaikumarbansal@gmail.com>
2019-05-27 02:04:32 -0600

    Made basic FoodTracker without NSCoding

---
+++ FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,41 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Jai Bansal on 5/24/19.
+//  Copyright © 2019 Jai Bansal. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+    
+    //MARK: Meal Class Tests
+    
+    // Confirm that the Meal initializer returns a Meal object wen passed valid parameters.
+    func testMealInitializationSucceds() {
+        // Zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+    
+    // Confirm that the Meal initilaizer returns nil when passed a negative rating or an empty name
+    func testMealInitializationFails() {
+        // Negative rating
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        // Empty String
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+    }
+}
